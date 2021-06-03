cfa52a0578c13fc40783a1463c5caa035e92164b
lowco972 <gregory.vaste@gmail.com>
2021-06-03 17:27:07 +0200

    traduction et premier integration dans laboutique avec fake img

--- app/Http/Controllers/Shop/ProductsController.php
+++ app/Http/Controllers/Shop/ProductsController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\Shop;
 
 use App\Http\Controllers\Controller;
+use App\Models\Produit;
 use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
     public function Products(){
+        $produits = Produit::all();
         return view('shop/Products'); //->withTitle('nos produits');
     }
 }
--- database/seeders/ProduitsTableSeeder.php
+++ database/seeders/ProduitsTableSeeder.php
@@ -17,7 +17,63 @@ class ProduitsTableSeeder extends Seeder
         $produit -> nom_Produit = "Mug coucher de soleil";
         $produit -> description_produit = "Mug en ivore rare ";
         $produit -> prix_ht = 8.5;
-        $produit -> photo_produit = "uncaillou.jpg";
+        $produit -> photo_produit ="fakeimg1.jpg";
+        $produit -> save();
+
+        $produit = new \App\ProductsController();
+        $produit -> nom_Produit = "Mug coucher de soleil";
+        $produit -> description_produit = " ";
+        $produit -> prix_ht = 8.5;
+        $produit -> photo_produit = "fakeimg2.jpg";
+        $produit -> save();
+
+        $produit = new \App\ProductsController();
+        $produit -> nom_Produit = "Mug coucher de soleil";
+        $produit -> description_produit = "Mug en ivore rare ";
+        $produit -> prix_ht = 8.5;
+        $produit -> photo_produit = "fakeimg3.jpg";
+        $produit -> save();
+
+        $produit = new \App\ProductsController();
+        $produit -> nom_Produit = "Mug coucher de soleil";
+        $produit -> description_produit = "Mug en ivore rare ";
+        $produit -> prix_ht = 8.5;
+        $produit -> photo_produit = "fakeimg4.jpg";
+        $produit -> save();
+
+        $produit = new \App\ProductsController();
+        $produit -> nom_Produit = "Mug coucher de soleil";
+        $produit -> description_produit = "Mug en ivore rare ";
+        $produit -> prix_ht = 8.5;
+        $produit -> photo_produit = "fakeimg5.jpg";
+        $produit -> save();
+
+        $produit = new \App\ProductsController();
+        $produit -> nom_Produit = "Mug coucher de soleil";
+        $produit -> description_produit = "Mug en ivore rare ";
+        $produit -> prix_ht = 8.5;
+        $produit -> photo_produit = "fakeimg6.jpg";
+        $produit -> save();
+
+        $produit = new \App\ProductsController();
+        $produit -> nom_Produit = "Mug coucher de soleil";
+        $produit -> description_produit = "Mug en ivore rare ";
+        $produit -> prix_ht = 8.5;
+        $produit -> photo_produit = "fakeimg7.jpg";
+        $produit -> save();
+
+        $produit = new \App\ProductsController();
+        $produit -> nom_Produit = "Mug coucher de soleil";
+        $produit -> description_produit = "Mug en ivore rare ";
+        $produit -> prix_ht = 8.5;
+        $produit -> photo_produit = "fakeimg8.jpg";
+        $produit -> save();
+
+        $produit = new \App\ProductsController();
+        $produit -> nom_Produit = "Mug coucher de soleil";
+        $produit -> description_produit = "Mug en ivore rare ";
+        $produit -> prix_ht = 8.5;
+        $produit -> photo_produit = "fakeimg9.jpg";
         $produit -> save();
 
     };
