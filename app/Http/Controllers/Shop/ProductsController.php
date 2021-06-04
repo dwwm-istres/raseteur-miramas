e9e479b3ab5c2a30d005fa5cecf3c83b61ca08e3
lowco972 <gregory.vaste@gmail.com>
2021-06-04 02:02:17 +0200

    commit

--- app/Http/Controllers/Shop/ProductsController.php
+++ app/Http/Controllers/Shop/ProductsController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers\Shop;
 
 use App\Http\Controllers\Controller;
 use App\Models\Produit;
+use Database\Seeders\ProductsTableSeeder;
 use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
     public function Products(){
-        $produits = Produit::all();
-        return view('shop/Products'); //->withTitle('nos produits');
+        $products = Produit::all();
+        // dd($produits);
+        return view('shop/Products',compact('products')); //->withTitle('nos produits');
     }
 }
--- app/Models/post/Article.php
+++ app/Models/post/Article.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\post;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Produit extends Model
+class Article extends Model
 {
     use HasFactory;
 }
