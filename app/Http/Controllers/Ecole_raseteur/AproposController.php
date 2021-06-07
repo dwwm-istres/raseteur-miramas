e2d81400065693b1e093614f8a0d9c4fd1cc7235
lowco972 <gregory.vaste@gmail.com>
2021-06-07 16:57:39 +0200

    about

---
+++ app/Http/Controllers/Ecole_raseteur/AproposController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Ecole_raseteur;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AproposController extends Controller { 
+   public function Apropos() {
+      return view('ecole raseteur/aPropos');
+   } 
+}
