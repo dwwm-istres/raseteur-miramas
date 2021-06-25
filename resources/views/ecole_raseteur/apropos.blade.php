6f343942f0f705136e7797cc6ec350ba4e960a7c
nicolas <nicolas-simonet@live.fr>
2021-06-25 09:09:49 +0200

    Update apropos.blade.php

--- resources/views/ecole_raseteur/apropos.blade.php
+++ resources/views/ecole_raseteur/apropos.blade.php
@@ -1,16 +1,7 @@
 @Extends("layouts\app")
 @section("content")
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-    <link rel= "stylescheet" href="../ecole_raseteur/_style.scss">
-    <title>A Propos</title>
-</head>
-<body>
+
+
 <h1 style="text-align: center">Nos Actualités Articles</h1>                      
 <div class="card">
   <div class="card-body">
@@ -25,6 +16,5 @@
           <h2 style="text-align: center">Petite video d'exemple</h2>     
         <video id="video" src="{{asset('video/Le razet - Ecole Miramas.mp4')}}" autoplay loop muted ></video>
   </div>     
-</body>
-</html>
+  
 @stop
